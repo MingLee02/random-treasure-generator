5ffc26744529cad5bcc9829424e350f2ce2bc534
MingLee02 <ming.lee@hotmail.com>
2018-01-21 17:10:31 +0000

    Turn debug back on untill properfix is found.

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ail0hx+l@10#g#8e--ps72fvjvn&m+9s&#8fgc=vo5%g275e^7'
+DEBUG = True
 
 ALLOWED_HOSTS = ['frostgrave-treasure.herokuapp.com']
 
