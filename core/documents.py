bc7af6b9f3a313b4248f505ce81d87dc38f04a4a
ming lee <ming.lee@hotmail.com>
2018-12-24 20:52:01 +0000

    Add Equipment to search.

--- core/documents.py
+++ core/documents.py
@@ -7,11 +7,12 @@ client = Elasticsearch()
 
 my_search = Search(using=client)
 
-from frostgrave.models import Trinket
+from frostgrave.models import Trinket, Equipment
 
 connections.create_connection()
 
 trinket = Index('trinkets')
+equipment = Index('equipments')
 
 # reference elasticsearch doc for default settings here
 trinket.settings(
@@ -19,11 +20,24 @@ trinket.settings(
     number_of_replicas=0
 )
 
+equipment.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
+
 @trinket.doc_type
 class TrinketDocument(DocType):
     class Meta:
         model = Trinket
         fields = ['description', 'name', 'effect']
+
+
+@equipment.doc_type
+class EquipmentDocument(DocType):
+    class Meta:
+        model = Equipment
+        fields = ['description']
 
 
 # define simple search here
